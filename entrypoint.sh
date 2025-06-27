b722a17183b646c4a9eb34d04a4ea260c970b6b7
Snowy Jaguar (Teagan) <github@snowyjaguar.xyz>
2025-06-27 18:23:04 +0000

    normilases the secret name to match the enviroment variable expecations

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,15 @@ set -e
 for secret in /run/secrets/*; do
   filename=$(basename "$secret")
   value=$(cat "$secret")
-  
-  # Remove prefix "adguardhomesync_" case-insensitive and convert to uppercase
-  varname=$(echo "$filename" | sed -E 's/^adguardhomesync_//I' | tr '[:lower:]' '[:upper:]')
 
-  export "$varname=$value"
+  # Normalize the secret name to uppercase
+  varname=$(echo "$filename" | tr '[:lower:]' '[:upper:]')
+
+  # If prefixed with ADGUARDHOMESYNC_, strip it
+  clean_name=$(echo "$varname" | sed -E 's/^ADGUARDHOMESYNC_//I')
+
+  # Export only the clean, normalized name
+  export "$clean_name=$value"
 done
 
 exec "$@"
